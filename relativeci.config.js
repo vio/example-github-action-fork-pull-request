b26b744153e98f75e137e4791b94728bf72ce042
Vio <vio@beanon.com>
2021-08-20 21:33:55 +0200

    build: Use relative-ci/agent-action

--- relativeci.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  payloadFilepath: './artifacts/relative-ci-agent.json',
-  includeCommitMessage: true,
-  webpack: {
-    // Path to Webpack stats JSON file
-    stats: './artifacts/webpack-stats.json'
-  }
-}
